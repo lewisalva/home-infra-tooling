8d5ceea1c87dcef79af9b950372fbcac8196c8ec
Lewis Alva <lewis@j1.support>
2024-04-14 22:48:12 -0700

    simplify auth middleware

--- server-elysia-pw-auth/src/globalMiddleware/authentication.ts
+++ server-elysia-pw-auth/src/globalMiddleware/authentication.ts
@@ -1,6 +1,5 @@
 import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
 import { Elysia } from 'elysia';
-import type { Session, User } from 'lucia';
 import { Lucia, verifyRequestOrigin } from 'lucia';
 
 import { sessionsTable, usersTable } from '../schema';
@@ -34,76 +33,46 @@ declare module 'lucia' {
 }
 
 export const ensureAuthentication = new Elysia()
-  .derive(
-    { as: 'scoped' },
-    async (
-      context
-    ): Promise<{
-      user: User | null;
-      session: Session | null;
-    }> => {
-      if (context.request.method !== 'GET') {
-        const originHeader = context.request.headers.get('Origin');
-        const hostHeader = context.request.headers.get('Host');
-        if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader])) {
-          return {
-            user: null,
-            session: null,
-          };
-        }
+  .derive({ as: 'scoped' }, async ({ cookie, error, headers, request }) => {
+    if (request.method !== 'GET') {
+      const originHeader = headers['origin'];
+      const hostHeader = headers['host'];
+      if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader])) {
+        return error(401);
       }
-
-      const cookieHeader = context.request.headers.get('Cookie') ?? '';
-      const sessionId = lucia.readSessionCookie(cookieHeader);
-      if (!sessionId) {
-        return {
-          user: null,
-          session: null,
-        };
-      }
-
-      const { session, user } = await lucia.validateSession(sessionId);
-      if (session && session.fresh) {
-        const sessionCookie = lucia.createSessionCookie(session.id);
-        context.cookie[sessionCookie.name].set({
-          value: sessionCookie.value,
-          ...sessionCookie.attributes,
-        });
-      }
-      if (!session) {
-        const sessionCookie = lucia.createBlankSessionCookie();
-        context.cookie[sessionCookie.name].set({
-          value: sessionCookie.value,
-          ...sessionCookie.attributes,
-        });
-      }
-      return {
-        user,
-        session,
-      };
     }
-  )
-  .onBeforeHandle({ as: 'scoped' }, ({ error, user }) => {
-    if (user === null) {
+
+    const cookieHeader = headers['cookie'] ?? '';
+    const sessionId = lucia.readSessionCookie(cookieHeader);
+    if (!sessionId) {
       return error(401);
     }
-  })
-  .derive(
-    { as: 'scoped' },
-    (
-      context
-    ): {
-      user: User;
-      session: Session;
-    } => {
-      return {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        user: context.user!,
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        session: context.session!,
-      };
+
+    const { session, user } = await lucia.validateSession(sessionId);
+    if (!(session && user)) {
+      if (!session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookie[sessionCookie.name].set({
+          value: sessionCookie.value,
+          ...sessionCookie.attributes,
+        });
+      }
+      return error(401);
     }
-  )
+
+    if (session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(session.id);
+      cookie[sessionCookie.name].set({
+        value: sessionCookie.value,
+        ...sessionCookie.attributes,
+      });
+    }
+
+    return {
+      user,
+      session,
+    };
+  })
   .macro(({ onBeforeHandle }) => ({
     ensurePlatformAdmin(_value: boolean) {
       onBeforeHandle(({ user, error }) => {
